d8bb719dfe9577771a55a604230159c90c86dad3
Chunhua Liao <liao6@lnl.gov>
2021-07-28 16:34:33 -0700

    SM gpu version only inner loop has pragmas

--- test/run-mm.sh
+++ test/run-mm.sh
@@ -7,7 +7,11 @@ HARDWAREE_NAME=`uname -m`
 make clean
 make ./$EXE_FILE
 
-export OMP_NUM_THREADS=88
+#using the physical core count as default thread count, avoiding hyper threading
+PHYSICAL_CORE_COUNT=`lscpu -p | egrep -v '^#' | sort -u -t, -k 2,4 | wc -l`
+export OMP_NUM_THREADS=$PHYSICAL_CORE_COUNT
+echo "Set to physical core count: OMP_NUM_THREADS="$OMP_NUM_THREADS
+
 # let the program set it automatically?
 # run 50 times to collect enough training data
 # 10k to 1000k, step 20k
@@ -20,5 +24,5 @@ do
  echo "running count=$counter, problem size=$size"
 
 # make the trace folder name more descriptive:  machine, compiler, thread count, program, timestamp 
-  APOLLO_TRACE_FOLDER_SUFFIX="-$NODE_NAME-$HARDWAREE_NAME-clang-12.0.0-$OMP_NUM_THREADS-threads-$EXE_FILE-$TIMESTAMP" APOLLO_CROSS_EXECUTION=1 ./$EXE_FILE $size
+  APOLLO_TRACE_FOLDER_SUFFIX="-$NODE_NAME-clang-12.0.0-$OMP_NUM_THREADS-threads-$EXE_FILE-$TIMESTAMP" APOLLO_CROSS_EXECUTION=1 ./$EXE_FILE $size
 done
